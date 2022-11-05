ee5fc59975792789a8c9dd188dfce9168f35eb90
dragonyc1002 <mivck1012@gmail.com>
2022-11-05 22:21:56 +0800

    chore: change indentations of the whole project

--- src/components/sound.js
+++ src/components/sound.js
@@ -1,72 +1,69 @@
 class Sound {
-	constructor(parent){
-		this.parent = parent;
-		this.sounds = [];
-		this.muted = true;
-	}
+  constructor(parent){
+    this.parent = parent;
+    this.sounds = [];
+    this.muted = true;
+  }
 
-	create(src, id, loop = false){
-		let audio = document.createElement("audio");
-		audio.src = src;
-		audio.id = id;
-		audio.muted = true;
-		this.sounds.push(audio);
-		this.parent.append(audio);
+  create(src, id, loop = false){
+    let audio = document.createElement("audio");
+    audio.src = src;
+    audio.id = id;
+    audio.muted = true;
+    this.sounds.push(audio);
+    this.parent.append(audio);
 
-		if(loop){
-			audio.setAttribute("loop", "");
-		}
+    if(loop){
+      audio.setAttribute("loop", "");
+    }
+    return audio;
+  }
 
-		return audio;
-	}
+  soundSetting(){
+    let soundItems = document.querySelectorAll(".sound-item");
+    for(let soundItem of soundItems){
+      soundItem.addEventListener("click", (e)=>{
+        this.muteToggle();
+      });
+    }
+  }
 
-}
+  muteToggle(){
+    if(!this.muted){
+      for(let sound of this.sounds){
+        sound.muted = true;
+      }
+      document.querySelector("#sound-speaker").innerHTML = "\u{1F507}";
+      document.querySelector("#sound-description").innerHTML = "off";
+      this.muted = true;
+    } else {
+      for(let sound of this.sounds){
+        sound.muted = false;
+      }
+      document.querySelector("#sound-speaker").innerHTML = "\u{1F509}";
+      document.querySelector("#sound-description").innerHTML = "on";
+      this.muted = false;
+    }
+  }
 
-Sound.prototype.soundSetting = function(){
-	let soundItems = document.querySelectorAll(".sound-item");
-	for(let soundItem of soundItems){
-		soundItem.addEventListener("click", (e)=>{
-			this.muteToggle();
-		});
-	}
-};
+  pause(){
+    for(let sound of this.sounds){
+      sound.pause();
+    }
+  }
 
-Sound.prototype.muteToggle = function(){
-	if(!this.muted){
-		for(let sound of this.sounds){
-			sound.muted = true;
-		}
-		document.querySelector("#sound-speaker").innerHTML = "\u{1F507}";
-		document.querySelector("#sound-description").innerHTML = "off";
-		this.muted = true;
-	} else {
-		for(let sound of this.sounds){
-			sound.muted = false;
-		}
-		document.querySelector("#sound-speaker").innerHTML = "\u{1F509}";
-		document.querySelector("#sound-description").innerHTML = "on";
-		this.muted = false;
-	}
-};
-
-Sound.prototype.pause = function(){
-	for(let sound of this.sounds){
-		sound.pause();
-	}
-}
-
-Sound.prototype.play = function(){
-	for(let sound of this.sounds){
-		sound.play();
-	}
-}
+  play(){
+    for(let sound of this.sounds){
+      sound.play();
+    }
+  }
 
 let sound = new Sound(document.querySelector("#sound-div")),
-	backgroundSound = sound.create("./Love_Letter.mp3", "background_sound", true),
-	movesSound = sound.create("./moves.mp3", "moves_sound"),
-	dropSound = sound.create("./drop.mp3", "drop_sound"),
-	pointsSound = sound.create("./points.mp3", "points_sound"),
-	finishSound = sound.create("./finish.mp3", "finish_sound");;
+  backgroundSound = sound.create("./src/assets/sounds/Love_Letter.mp3", "background_sound", true),
+  movesSound = sound.create("./src/assets/sounds/moves.mp3", "moves_sound"),
+  dropSound = sound.create("./src/assets/sounds/drop.mp3", "drop_sound"),
+  pointsSound = sound.create("./src/assets/sounds/points.mp3", "points_sound"),
+  finishSound = sound.create("./src/assets/sounds/finish.mp3", "finish_sound");;
 
 sound.muteToggle();
 sound.soundSetting();
